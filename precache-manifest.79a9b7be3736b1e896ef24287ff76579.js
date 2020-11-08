cef0121a76a9e4d71377b7451e9888afe92d3b6d
Shashwat Doshi <doshishashwat17@gmail.com>
2020-11-08 18:54:01 +0530

    Updates

---
+++ precache-manifest.79a9b7be3736b1e896ef24287ff76579.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8627e50e07ca185859f6be99f36c323a",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "8ff17261d117e12d7278",
+    "url": "/home/static/css/main.022eb82c.chunk.css"
+  },
+  {
+    "revision": "f08f5c2faeba83ba75d8",
+    "url": "/home/static/js/2.3b189aff.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.3b189aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ff17261d117e12d7278",
+    "url": "/home/static/js/main.ef04fe6f.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "2cad743faacd4a37d9c9582448eadc68",
+    "url": "/home/static/media/resume.2cad743f.pdf"
+  },
+  {
+    "revision": "d824bd86be6cca283bc99f47cee0674a",
+    "url": "/home/static/media/shashwatdoshi.d824bd86.png"
+  }
+]);
